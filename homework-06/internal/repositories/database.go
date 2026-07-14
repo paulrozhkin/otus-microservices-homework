9b582dbedff06104b7580216cee3879a8ee64db5
paulrozhkin <blackiiifox@gmail.com>
2026-07-14 23:22:49 +0300

    add homework-06 without changes

---
+++ homework-06/internal/repositories/database.go
@@ -0,0 +1,40 @@
+package repositories
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/paulrozhkin/otus-microservices-homework/otus-microservices-homework-06/internal/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type HealthChecker interface {
+	Ping(ctx context.Context) error
+}
+
+type DBHealthChecker struct {
+	db *gorm.DB
+}
+
+func NewDbConnection(cfg config.DBConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=%s",
+		cfg.Host, cfg.User.Value(), cfg.Password.Value(),
+		cfg.DBName, cfg.Port, cfg.SSLMode, cfg.TimeZone,
+	)
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
+
+func NewDBHealthChecker(db *gorm.DB) *DBHealthChecker {
+	return &DBHealthChecker{db: db}
+}
+
+func (h *DBHealthChecker) Ping(ctx context.Context) error {
+	sqlDB, err := h.db.DB()
+	if err != nil {
+		return err
+	}
+
+	return sqlDB.PingContext(ctx)
+}
